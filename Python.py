9a6f8b5872e07ae5430f940b529d954055e8c998
AnuragSharma-Ravulapally <118966154+AnuragSharma-Ravulapally@users.noreply.github.com>
2022-12-07 14:20:28 +0530

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,9 @@
+import math
+print("Maximum number :",max(1,2,3,4,5,6))
+print("Minimum number :",min(1,2,3,4,5,6))
+print("Power of function :",pow(2,3))
+print("Absolute value of function is :",abs(212132))  #absolute means mode of any number here negative value change into positive
+print("Rounding of the number: ",round(13.1))
+print("Floor value of number is: ",math.floor(12.9))   #always gives lower value
+print("Ceil value of a number: ",math.ceil(12.01))     #always gives upper value
+print("Value of Sin Function is: ",math.sin(90))
